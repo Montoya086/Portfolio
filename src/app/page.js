2ba29ee08c008a225cce29cdec508a2c97cedbab
Montoya086 <mon21552@uvg.edu.gt>
2023-05-21 21:49:24 -0600

    header

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,36 @@
+"use client"
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.scss'
+import { useState } from 'react'
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='app-content'>
+      <div className='header-wrapper'>
+        <div className='header'>
+          <div className='header-left'>
+            <h4>Andrés Montoya</h4>
+            <div className='underline'>
+
+            </div>
+          </div>
+          <div className='header-right'>
+            <div className='menu-button' onClick={()=>setIsOpen(!isOpen)}>
+              <div className={`menu-button-bar element1 ${isOpen ? "open" : "close"}`}/>
+              <div className={`menu-button-bar element2 ${isOpen ? "open" : "close"}`}/>
+              <div className={`menu-button-bar element3 ${isOpen ? "open" : "close"}`}/>
+            </div>
+            <div className='menu-wrapper'>
+            
+            </div>
+          </div>
         </div>
       </div>
+      <div className='test'>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
