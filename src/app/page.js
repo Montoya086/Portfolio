2bc23bd477d3227e140d77f1d2ab6dbd2ff57e61
Montoya086 <mon21552@uvg.edu.gt>
2023-05-21 22:46:40 -0600

    header update

--- src/app/page.js
+++ src/app/page.js
@@ -2,32 +2,14 @@
 import Image from 'next/image'
 import styles from './page.scss'
 import { useState } from 'react'
+import Header from './header'
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <div className='app-content'>
-      <div className='header-wrapper'>
-        <div className='header'>
-          <div className='header-left'>
-            <h4>Montoya's Portfolio</h4>
-            <div className='underline'>
-
-            </div>
-          </div>
-          <div className='header-right'>
-            <div className='menu-button' onClick={()=>setIsOpen(!isOpen)}>
-              <div className={`menu-button-bar element1 ${isOpen ? "open" : "close"}`}/>
-              <div className={`menu-button-bar element2 ${isOpen ? "open" : "close"}`}/>
-              <div className={`menu-button-bar element3 ${isOpen ? "open" : "close"}`}/>
-            </div>
-            <div className={`menu-wrapper ${isOpen ? "open" : "close"}`}>
-
-            </div>
-          </div>
-        </div>
-      </div>
+      <Header isOpen={isOpen} setIsOpen={setIsOpen}/>
       <div className='body-wrapper'>
         <div className={`body-overlay ${isOpen ? "open" : "close"}`} onClick={()=>setIsOpen(false)}/>
       </div>
