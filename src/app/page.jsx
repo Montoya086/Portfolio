ad2bed6320f999450ef70e7fafb871980bc7000a
Montoya086 <mon21552@uvg.edu.gt>
2023-05-22 15:28:18 -0600

    home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from 'next/image'
-import styles from './page.scss'
+import './page.scss'
 import { useState } from 'react'
 import Header from './header.jsx'
 
@@ -12,6 +12,25 @@ export default function Home() {
       <Header isOpen={isOpen} setIsOpen={setIsOpen}/>
       <div className='body-wrapper'>
         <div className={`body-overlay ${isOpen ? "open" : "close"}`} onClick={()=>setIsOpen(false)}/>
+        <div className='home-wrapper'>
+          <div className='stars'>
+            <div className='shifting-stars'>
+              <div className='star'/>
+              <div className='star'/>
+              <div className='star'/>
+              <div className='star'/>
+              <div className='star'/>
+              <div className='star'/>
+              <div className='star'/>
+              <div className='star'/>
+            </div>
+          </div>
+          <div className='earth-moon'>
+            <img className="earth-image" src="http://salehriaz.com/404Page/img/earth.svg" width="100px" alt=''/>
+            <img className="moon-image" src="http://salehriaz.com/404Page/img/moon.svg" width="80px" alt=''/>
+          </div>
+          <img className="astronaut-image" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px" alt=''/>
+        </div>
       </div>
     </div>
   )
