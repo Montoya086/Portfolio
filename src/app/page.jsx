15c0504ff5317aa26c7f600c920872f3aa79e6e3
Montoya086 <mon21552@uvg.edu.gt>
2023-05-22 15:50:30 -0600

    animation text

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,22 @@ import Header from './header.jsx'
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false)
+  const [earth, setEarth] = useState(false)
+  const [astro, setAstro] = useState(false)
+
+  const handleEarth =()=>{
+    setEarth(true)
+    setTimeout(()=>{
+      setEarth(false)
+    }, 5000)
+  }
+
+  const handleAstro =()=>{
+    setAstro(true)
+    setTimeout(()=>{
+      setAstro(false)
+    }, 5000)
+  }
 
   return (
     <div className='app-content'>
@@ -26,10 +42,14 @@ export default function Home() {
             </div>
           </div>
           <div className='earth-moon'>
-            <img className="earth-image" src="http://salehriaz.com/404Page/img/earth.svg" width="100px" alt=''/>
+            <img className="earth-image" src="http://salehriaz.com/404Page/img/earth.svg" width="100px" alt='' onClick={handleEarth}/>
             <img className="moon-image" src="http://salehriaz.com/404Page/img/moon.svg" width="80px" alt=''/>
+            <h4 className={`home-text ${earth ? "shown" : ""}`}>That's Home!</h4>
           </div>
-          <img className="astronaut-image" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px" alt=''/>
+          <div className='astronaut'>
+            <img className="astronaut-image" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px" alt='' onClick={handleAstro}/>
+            <h4 className={`astronaut-text ${astro ? "shown" : ""}`}>Hello friend!</h4>
+          </div>
         </div>
       </div>
     </div>
